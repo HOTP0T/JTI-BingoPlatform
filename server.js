607c60104d00e25a0580ad4ae01fe9b8a1124946
HOTPOT <tpcmaky@gmail.com>
2024-07-05 11:51:03 +0200

    adding render server2

--- server.js
+++ server.js
@@ -27,5 +27,6 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running in ${NODE_ENV} mode on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
