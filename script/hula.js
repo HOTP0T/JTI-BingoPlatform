a87de81856edc5669c5ad272f0e0b9cfefd9fa03
HOTPOT <tpcmaky@gmail.com>
2024-07-15 07:20:13 +0200

    removed unecessary files

---
+++ script/hula.js
@@ -0,0 +1,21 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  var password = document.getElementById('password').value;
+  var redirectUrl;
+
+  // Define your password-URL mappings
+  const passwordMappings = {
+      'password1': '../pages/CardID1.html',
+      'password2': '../pages/CardID2.html',
+      // Add more password-URL pairs as needed
+  };
+
+    // Check if the entered password exists in the mappings
+    if (passwordMappings[password]) {
+      redirectUrl = passwordMappings[password];
+      window.location.href = redirectUrl; // Redirect to the specified URL
+  } else {
+      alert('Invalid password');
+  }
+});
